0ad80cce78d3e5db71f9bf95819ab2fbaaa917ff
washington007 <washo968@gmail.com>
2022-05-16 15:44:22 -0500

    Se aplico los ultimos cambios

---
+++ resources/views/articulo/edit.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.plantillabase')
+
+@section('contenido')
+<h2>Edita un Plato</h2>
+
+<form action="/articulos/{{$articulo->id}}" method="POST">
+    @csrf
+    @method('PUT')
+
+    <div class="mb-3">
+        <label for="" class="form-label">Codigo</label>
+        <input id="codigo" name="codigo" type="text" class="form-control" value="{{$articulo->codigo}}">
+    </div>
+
+    <div class="mb-3">
+        <label for="" class="form-label">Descripcion</label>
+        <input id="descripcion" name="descripcion" type="text" class="form-control" value="{{$articulo->descripcion}}">
+    </div>
+
+    <div class="mb-3">
+        <label for="" class="form-label">Cantidad</label>
+        <input id="cantidad" name="cantidad" type="number" class="form-control" value="{{$articulo->cantidad}}">
+    </div>
+
+    <div class="mb-3">
+        <label for="" class="form-label">Precio</label>
+        <input id="precio" name="precio" type="number" step="any" value="0.00" class="form-control" value="{{$articulo->precio}}">
+    </div>
+
+    <a href="/articulos" class="btn btn-secondary" tabindex="5">Cancelar</a>
+    <button type="submit" class="btn btn-primary" tabindex="4">Guardar</button>
+</form>
+@endsection
---
+++ resources/views/layouts/plantillabase.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-CuOF+2SnTUfTwSZjCXf01h7uYhfOBuxIhGKPbfEJ3+FqH/s6cIFN9bGr1HmAg4fQ" crossorigin="anonymous">
+
+    <title>CRUD con Laravel </title>
+  </head>
+  <body>
+    <div class="container">
+    <h1 class="bg-primary text-white text-center mb-3">Bienvenido al Restaurante Don Fernan</h1>
+
+
+    <div class="container">
+        @yield('contenido')
+    </div>  
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-popRpmFF9JQgExhfw5tZT4I9/CI5e2QcuUZPOVXb1m7qUmeR2b50u+YFEYe1wgzy" crossorigin="anonymous"></script>
+
+  </body>
+</html>
